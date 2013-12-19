698c450a86535579d81b16705ad775f0f4ac21ea
dirtshell <dirt1996@vm-0.dirt1996.koding.kd.io>
2013-12-19 01:09:27 +0000

    Region is determined by timezone, and is now fully implemented

--- model.py
+++ model.py
@@ -5,7 +5,6 @@ import pytz
 
 db = web.database(dbn='sqlite', db='smashbook.db')
 
-
 def getMatches():
     return db.select('matches', order='id')
     
@@ -15,7 +14,7 @@ def getMatch(id):
     except IndexError:
         return None
         
-def newMatch(title, net_code, password=None):
+def newMatch(title, net_code, timezone, password=None):
     unique_key = str(uuid.uuid4()).upper().replace('-','')  # Generate a unique key
     unique_key = unique_key[0:10]   # Truncate to a 10 char long random string
     ip = web.ctx['ip']   # Get the IP of the request
--- smashonline.py
+++ smashonline.py
@@ -3,6 +3,81 @@ from web import form
 import model
 from captcha import getCaptcha
 
+# List of valid time zones
+olsonDB = [
+    'Pacific/Majuro',
+    'Pacific/Pago_Pago',
+    'America/Adak',
+    'Pacific/Honolulu',
+    'Pacific/Marquesas',
+    'Pacific/Gambier',
+    'America/Anchorage',
+    'America/Los_Angeles',
+    'Pacific/Pitcairn',
+    'America/Phoenix',
+    'America/Denver',
+    'America/Guatemala',
+    'America/Chicago',
+    'Pacific/Easter',
+    'America/Bogota',
+    'America/New_York',
+    'America/Caracas',
+    'America/Halifax',
+    'America/Santo_Domingo',
+    'America/Santiago',
+    'America/St_Johns',
+    'America/Godthab',
+    'America/Argentina/Buenos_Aires',
+    'America/Montevideo',
+    'America/Noronha',
+    'America/Noronha',
+    'Atlantic/Azores',
+    'Atlantic/Cape_Verde',
+    'UTC',
+    'Europe/London',
+    'Europe/Berlin',
+    'Africa/Lagos',
+    'Africa/Windhoek',
+    'Asia/Beirut',
+    'Africa/Johannesburg',
+    'Asia/Baghdad',
+    'Europe/Moscow',
+    'Asia/Tehran',
+    'Asia/Dubai',
+    'Asia/Baku',
+    'Asia/Kabul',
+    'Asia/Yekaterinburg',
+    'Asia/Karachi',
+    'Asia/Kolkata',
+    'Asia/Kathmandu',
+    'Asia/Dhaka',
+    'Asia/Omsk',
+    'Asia/Rangoon',
+    'Asia/Krasnoyarsk',
+    'Asia/Jakarta',
+    'Asia/Shanghai',
+    'Asia/Irkutsk',
+    'Australia/Eucla',
+    'Australia/Eucla',
+    'Asia/Yakutsk',
+    'Asia/Tokyo',
+    'Australia/Darwin',
+    'Australia/Adelaide',
+    'Australia/Brisbane',
+    'Asia/Vladivostok',
+    'Australia/Sydney',
+    'Australia/Lord_Howe',
+    'Asia/Kamchatka',
+    'Pacific/Noumea',
+    'Pacific/Norfolk',
+    'Pacific/Auckland',
+    'Pacific/Tarawa',
+    'Pacific/Chatham',
+    'Pacific/Tongatapu',
+    'Pacific/Apia',
+    'Pacific/Kiritimati'
+]
+
 render = web.template.render('templates/')
 
 urls = (
@@ -31,6 +106,7 @@ matchForm = form.Form(
         form.Validator("Title too long", lambda i: len(i.title) <= 25), # Check to make sure the title is within 25 characters
         form.Validator("Invalid password", lambda i: len(i.password) <=25),  # Check to make sure the password isn't too long
         form.Validator("Title is required", lambda i: len(i.title) != 0),   # Check to make sure a title was entered
+        form.Validator("You shouldn't see this error", lambda i:  i.timezone in olsonDB),   # Check to make sure the time zone is valid
         form.Validator("Incorrect CAPTCHA", lambda i: i.captcha == session.captcha),]    # Check the CAPTCHA they entered against the one in the session variable
     )
 
@@ -53,7 +129,7 @@ class create_match:
             return render.create(form)    # Return them to the create page
         else:   # Otherwise save it
             # form.d.boe and form['boe'].value are both ways to extract data
-            model.newMatch(title=form.d.title, net_code=form.d.net_code, password=form.d.password)
+            model.newMatch(title=form.d.title, net_code=form.d.net_code, password=form.d.password, timezone=form.d.timezone)
             raise web.seeother('/')   # Send em to the home page
             
 class captcha:
